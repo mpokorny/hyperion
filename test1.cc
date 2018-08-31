a99b769bc15af6a5c1d8d6f6bb43aa0236f953cf
Martin Pokorny <mpokorny@nrao.edu>
2018-08-31 14:14:58 -0600

    Change offspring_rects() to offspring_index_space()
    IOW, return an index space directly instead of a vector of Rect. Removes need
    for one serializer/deserializer (and feels more natural in Legion)

--- test1.cc
+++ test1.cc
@@ -18,14 +18,13 @@ enum TaskIDs {
 enum FieldIDs {
   FID_ENVELOPE,
   FID_TREE,
-  FID_VRECT,
+  FID_INDEX_SPACE,
 
   FID_PART_COLOR,
 };
 
 enum SerdezIDs {
   SID_TREE = 1,
-  SID_VRECT = 2,
 };
 
 typedef Tree<coord_t> TreeL;
@@ -57,54 +56,9 @@ public:
   }
 };
 
-template <int DIM>
-class vrect_serdez {
-public:
-  typedef std::vector<Rect<DIM>> FIELD_TYPE;
-
-  static const size_t MAX_SERIALIZED_SIZE = std::numeric_limits<size_t>::max();
-
-  static size_t
-  serialized_size(const std::vector<Rect<DIM>>& val) {
-    return val.size() * sizeof(Rect<DIM>) + sizeof(size_t);
-  }
-
-  static size_t
-  serialize(const std::vector<Rect<DIM>>& val, void *buffer) {
-    unsigned char *b = reinterpret_cast<unsigned char *>(buffer);
-    size_t vlen = val.size();
-    *reinterpret_cast<size_t *>(b) = vlen;
-    b += sizeof(vlen);
-    size_t rectsz = sizeof(Rect<DIM>);
-    for (size_t i = 0; i < vlen; ++i) {
-      memcpy(b, &val[i], rectsz);
-      b += rectsz;
-    }
-    return serialized_size(val);
-  }
-
-  static size_t
-  deserialize(std::vector<Rect<DIM>>& val, const void *buffer) {
-    const unsigned char *b = reinterpret_cast<const unsigned char *>(buffer);
-    size_t vlen = *reinterpret_cast<const size_t *>(b);
-    b += sizeof(vlen);
-    size_t rectsz = sizeof(Rect<DIM>);
-    val.clear();
-    for (size_t i = 0; i < vlen; ++i) {
-      val.push_back(*reinterpret_cast<const Rect<DIM> *>(b));
-      b += rectsz;
-    }
-    return serialized_size(val);
-  }
-
-  static void
-  destroy(std::vector<Rect<DIM>>&) {
-  }
-};
-
 template <int DIM, typename CB>
-std::vector<Rect<DIM>>
-offspring_rects(ssize_t n, CB cbfn, Context ctx, Runtime* runtime);
+IndexSpaceT<DIM>
+offspring_index_space(ssize_t n, CB cbfn, Context ctx, Runtime* runtime);
 
 template <int DIM>
 class TreeSpaceTask
@@ -137,7 +91,7 @@ public:
       WRITE_DISCARD,
       EXCLUSIVE,
       child_output_lr);
-    child_output_req.add_field(FID_VRECT);
+    child_output_req.add_field(FID_INDEX_SPACE);
     add_region_requirement(child_output_req);
   }
 
@@ -169,11 +123,11 @@ public:
       false> trees(regions[0], FID_TREE);
     const FieldAccessor<
       WRITE_DISCARD,
-      std::vector<Rect<DIM>>,
+      IndexSpaceT<DIM>,
       1,
       coord_t,
-      Realm::AffineAccessor<std::vector<Rect<DIM>>,1,coord_t>,
-      false> domains(regions[1], FID_VRECT);
+      Realm::AffineAccessor<IndexSpaceT<DIM>,1,coord_t>,
+      false> ispaces(regions[1], FID_INDEX_SPACE);
 
     Rect<1> rect = runtime->get_index_space_domain(
       ctx,
@@ -194,11 +148,12 @@ public:
       for (size_t i = 0; i < tree_rect.size(); ++i)
         std::tie(tree_rect_lo[i + fixd], tree_rect_hi[i + fixd]) = tree_rect[i];
       if (tree.is_array()) {
-        domains[*pir] =
-          {Rect<DIM>(Point<DIM>(tree_rect_lo), Point<DIM>(tree_rect_hi))};
+        ispaces[*pir] = runtime->create_index_space(
+          ctx,
+          Rect<DIM>(Point<DIM>(tree_rect_lo), Point<DIM>(tree_rect_hi))) ;
       } else {
         auto children = tree.children();
-        domains[*pir] = offspring_rects<DIM>(
+        ispaces[*pir] = offspring_index_space<DIM>(
           children.size(),
           [&](Point<1> chidx, Rect<DIM>& rect, TreeL& tr) {
             coord_t i, n;
@@ -206,8 +161,7 @@ public:
             std::tie(i, n, t) = children[chidx];
             tree_rect_lo[fixd] = i;
             tree_rect_hi[fixd] = i + n - 1;
-            rect =
-              Rect<DIM>(Point<DIM>(tree_rect_lo), Point<DIM>(tree_rect_hi));
+            rect = Rect<DIM>(Point<DIM>(tree_rect_lo), Point<DIM>(tree_rect_hi));
             tr = t;
           },
           ctx,
@@ -276,36 +230,30 @@ public:
     : InlineLauncher(
       RegionRequirement(
         lr,
-        {FID_VRECT},
-        {FID_VRECT},
+        {FID_INDEX_SPACE},
+        {FID_INDEX_SPACE},
         READ_ONLY,
         EXCLUSIVE,
         lr)),
       m_nch(n) {
   }
 
-  std::vector<Rect<DIM>>
+  IndexSpaceT<DIM>
   dispatch(Context ctx, Runtime* runtime) {
     PhysicalRegion region = runtime->map_region(ctx, *this);
     const FieldAccessor<
       READ_ONLY,
-      std::vector<Rect<DIM>>,
+      IndexSpaceT<DIM>,
       1,
       coord_t,
-      Realm::AffineAccessor<std::vector<Rect<DIM>>,1,coord_t>,
-      false> acc_rect(region, FID_VRECT);
+      Realm::AffineAccessor<IndexSpaceT<DIM>,1,coord_t>,
+      false> acc_ispace(region, FID_INDEX_SPACE);
 
-    std::vector<Rect<DIM>> result;
-    auto result_in = std::back_inserter(result);
-    for (coord_t i = 0; i < m_nch; ++i) {
-      const std::vector<Rect<DIM>>& vr = acc_rect[i];
-      std::copy(
-        vr.begin(),
-        vr.end(),
-        result_in);
-    }
+    std::vector<IndexSpaceT<DIM>> ispaces;
+    for (coord_t i = 0; i < m_nch; ++i)
+      ispaces.push_back(acc_ispace[i]);
     runtime->unmap_region(ctx, region);
-    return result;
+    return runtime->union_index_spaces(ctx, ispaces);;
   }
 
   ssize_t m_nch;
@@ -417,8 +365,8 @@ public:
 };
 
 template <int DIM, typename CB>
-std::vector<Rect<DIM>>
-offspring_rects(ssize_t n, CB cbfn, Context ctx, Runtime* runtime) {
+IndexSpaceT<DIM>
+offspring_index_space(ssize_t n, CB cbfn, Context ctx, Runtime* runtime) {
 
   Rect<1> child_is_rect = Rect<1>(Point<1>(0), Point<1>(n - 1));
   IndexSpaceT<1> child_is =
@@ -433,22 +381,18 @@ offspring_rects(ssize_t n, CB cbfn, Context ctx, Runtime* runtime) {
   LogicalRegion child_input_lr =
     runtime->create_logical_region(ctx, child_is, child_input_fs);
 
+  ChildInputInitTask<DIM> init(child_input_lr, n);
+  init.dispatch(cbfn, ctx, runtime);
+
   FieldSpace child_output_fs = runtime->create_field_space(ctx);
   {
     auto fa = runtime->create_field_allocator(ctx, child_output_fs);
-    fa.allocate_field(
-      sizeof(std::vector<Rect<DIM>>),
-      FID_VRECT,
-      SID_VRECT);
+    fa.allocate_field(sizeof(IndexSpaceT<DIM>), FID_INDEX_SPACE);
   }
   LogicalRegion child_output_lr =
     runtime->create_logical_region(ctx, child_is, child_output_fs);
 
-  ChildInputInitTask<DIM> init(child_input_lr, n);
-  init.dispatch(cbfn, ctx, runtime);
-
-  auto child_cs = runtime->create_index_space(ctx, child_is_rect);
-  auto child_ip = runtime->create_equal_partition(ctx, child_is, child_cs);
+  auto child_ip = runtime->create_equal_partition(ctx, child_is, child_is);
   LogicalPartition child_input_lp =
     runtime->get_logical_partition(ctx, child_input_lr, child_ip);
   LogicalPartition child_output_lp =
@@ -458,16 +402,15 @@ offspring_rects(ssize_t n, CB cbfn, Context ctx, Runtime* runtime) {
     child_output_lp,
     child_input_lr,
     child_output_lr,
-    child_cs);
+    child_is);
   tree_space.dispatch(ctx, runtime);
 
   runtime->destroy_index_partition(ctx, child_ip);
   runtime->destroy_logical_partition(ctx, child_input_lp);
   runtime->destroy_logical_partition(ctx, child_output_lp);
-  runtime->destroy_index_space(ctx, child_cs);
 
   ChildOutputCollectTask<DIM> collect(child_output_lr, n);
-  std::vector<Rect<DIM>> result = collect.dispatch(ctx, runtime);
+  IndexSpaceT<DIM> result = collect.dispatch(ctx, runtime);
 
   runtime->destroy_logical_region(ctx, child_input_lr);
   runtime->destroy_logical_region(ctx, child_output_lr);
@@ -483,8 +426,8 @@ tree_index_space(const TreeL& tree, Context ctx, Runtime* runtime) {
   auto rank = tree.rank();
   assert(rank);
   assert(rank.value() == DIM);
-  std::vector<Rect<DIM>> domain =
-    offspring_rects<DIM>(
+  return
+    offspring_index_space<DIM>(
       1,
       [&](Point<1>, Rect<DIM>& rect, TreeL& tr) {
         auto env = tree.envelope();
@@ -496,7 +439,6 @@ tree_index_space(const TreeL& tree, Context ctx, Runtime* runtime) {
       },
       ctx,
       runtime);
-  return runtime->create_index_space(ctx, domain);
 }
 
 class TopLevelTask {
@@ -571,7 +513,6 @@ main(int argc, char* argv[]) {
   ReportIndexesTask<3, 1>::register_task();
 
   Runtime::register_custom_serdez_op<tree_serdez>(SID_TREE);
-  Runtime::register_custom_serdez_op<vrect_serdez<3>>(SID_VRECT);
 
   return Runtime::start(argc, argv);
 }
