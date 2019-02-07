6efc4b44c2d13e33c2550a1e3f9464289f4695ea
Martin Pokorny <mpokorny@nrao.edu>
2019-02-07 11:50:29 -0700

    Add legms_c.h
    Simplifies includes for C and Regent.

---
+++ legms/legms_c.h
@@ -0,0 +1,19 @@
+#ifndef LEGMS_C_H_
+#define LEGMS_C_H_
+
+#include "utility_c.h"
+#include "Grids_c.h"
+#include "Column_c.h"
+#include "ColumnPartition_c.h"
+#include "MSTable_c.h"
+#include "Table_c.h"
+#include "TableReadTask_c.h"
+
+#endif // LEGMS_C_H_
+
+// Local Variables:
+// mode: c
+// c-basic-offset: 2
+// fill-column: 80
+// indent-tabs-mode: nil
+// End:
