44ab0e9135dd56621f56f2fdd12eb887fe15c50e
Martin Pokorny <mpokorny@nrao.edu>
2019-03-11 10:20:47 -0600

    Unit test for TestLog

--- legms/testing/tests/log.cc
+++ legms/testing/tests/log.cc
@@ -7,25 +7,121 @@ using namespace Legion;
 
 enum {
   TEST_RUNNER_TASK_ID,
+  TEST_LOG_TASK_ID,
 };
+
+#define LOG_LENGTH 100
+
+void
+test_log_task(
+  const Task* task,
+  const std::vector<PhysicalRegion>& regions,
+  Context ctx,
+  Runtime *runtime) {
+
+  testing::TestLog<READ_WRITE> log(regions[0], regions[1], ctx, runtime);
+  size_t length = 0;
+  bool all_unknown = true;
+  log.for_each(
+    [&length, &all_unknown](auto& it) {
+      ++length;
+      all_unknown = all_unknown && (*it).state == testing::TestState::UNKNOWN;
+    });
+  testing::TestResult<READ_WRITE> test_result;
+  auto log_output = log.iterator();
+
+  test_result.state = (
+    (length == LOG_LENGTH)
+    ? testing::TestState::SUCCESS
+    : testing::TestState::FAILURE);
+  test_result.abort = false;
+  test_result.name = "Initial log size is correct";
+  if (test_result.state != testing::TestState::SUCCESS) {
+    std::ostringstream oss;
+    oss << length << " lines, rather than the expected " << LOG_LENGTH;
+    test_result.fail_info = oss.str();
+  }
+  log_output <<= test_result;
+  ++log_output;
+
+  test_result.state = (
+    all_unknown
+    ? testing::TestState::SUCCESS
+    : testing::TestState::FAILURE);
+  test_result.abort = false;
+  test_result.name = "Log initialized all results to UNKNOWN";
+  test_result.fail_info = "";
+  log_output <<= test_result;
+  ++log_output;
+
+  test_result.state = (
+    log.contains_abort()
+    ? testing::TestState::FAILURE
+    : testing::TestState::SUCCESS);
+  test_result.abort = false;
+  test_result.name = "Initial log not in ABORT state";
+  log_output <<= test_result;
+  ++log_output;
+
+  test_result.state = testing::TestState::SKIPPED;
+  test_result.abort = false;
+  test_result.name = "Expected SKIPPED";
+  log_output <<= test_result;
+  ++log_output;
+
+  test_result.state = testing::TestState::FAILURE;
+  test_result.abort = false;
+  test_result.name = "Expected FAILURE";
+  test_result.fail_info = "OK";
+  log_output <<= test_result;
+  ++log_output;
+
+  test_result.state = testing::TestState::FAILURE;
+  test_result.abort = true;
+  test_result.name = "Expected ABORT";
+  test_result.fail_info = "OK";
+  log_output <<= test_result;
+  ++log_output;
+
+  test_result.state = (
+    log.contains_abort()
+    ? testing::TestState::SUCCESS
+    : testing::TestState::FAILURE);
+  test_result.abort = false;
+  test_result.name = "Log in ABORT state after ABORT result";
+  test_result.fail_info = "";
+  log_output <<= test_result;
+  ++log_output;
+
+  if (test_result.state == testing::TestState::SUCCESS)
+    test_result.state = (
+      log.contains_abort()
+      ? testing::TestState::SUCCESS
+      : testing::TestState::FAILURE);
+  else
+    test_result.state = testing::TestState::SKIPPED;
+  test_result.abort = false;
+  test_result.name = "ABORT state is irreversible";
+  log_output <<= test_result;
+  ++log_output;
+}
 
 void
 test_runner_task(
   const Task*,
   const std::vector<PhysicalRegion>&,
-  Context ctx,
+  Context context,
   Runtime* runtime) {
 
   // initialize the test log
-  testing::TestLogReference logref(100, ctx, runtime);
-  testing::TestLog<WRITE_DISCARD>(logref).for_each(
-    [](auto& it) {
-      it <<= testing::TestResult<READ_ONLY>{
-        testing::TestState::UNKNOWN,
-          false,
-          "",
-          ""};
-    });
+  testing::TestLogReference logref(LOG_LENGTH, context, runtime);
+  testing::TestLog<WRITE_DISCARD>(logref).initialize();
+
+  TaskLauncher test(TEST_LOG_TASK_ID, TaskArgument());
+  auto reqs = logref.requirements<READ_WRITE>();
+  test.add_region_requirement(reqs[0]);
+  test.add_region_requirement(reqs[1]);
+  runtime->execute_task(context, test);
 
   // print out the test log
   std::ostringstream oss;
@@ -35,25 +131,27 @@ test_runner_task(
       switch (test_result.state) {
       case testing::TestState::SUCCESS:
         oss << "PASS: "
-            << test_result.location
+            << test_result.name
             << std::endl;
         break;
       case testing::TestState::FAILURE:
         oss << "FAIL: "
-            << test_result.location
-            << ": "
-            << test_result.description
-            << std::endl;
+            << test_result.name;
+        if (test_result.fail_info.size() > 0)
+          oss << ": "
+              << test_result.fail_info;
+        oss << std::endl;
         break;
       case testing::TestState::SKIPPED:
         oss << "SKIPPED: "
-            << test_result.location
+            << test_result.name
             << std::endl;
         break;
       case testing::TestState::UNKNOWN:
         break;
       }
     });
+  std::cout << oss.str();
 }
 
 int
@@ -66,6 +164,11 @@ main(int argc, char* argv[]) {
     TaskVariantRegistrar registrar(TEST_RUNNER_TASK_ID, "test_runner");
     registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
     Runtime::preregister_task_variant<test_runner_task>(registrar, "top_level");
+  }
+  {
+    TaskVariantRegistrar registrar(TEST_LOG_TASK_ID, "test_TestLog");
+    registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+    Runtime::preregister_task_variant<test_log_task>(registrar, "top_level");
   }
 
   return Runtime::start(argc, argv);
