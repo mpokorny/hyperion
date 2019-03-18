91ba7cba309ba8fe369b70674baff61c380217a4
Martin Pokorny <mpokorny@nrao.edu>
2019-03-18 11:21:40 -0600

    Complete IndexColumnTask unit test suite

--- legms/tests/utIndexColumnTask.cc
+++ legms/tests/utIndexColumnTask.cc
@@ -2,6 +2,8 @@
 #include "testing/TestRecorder.h"
 
 #include <memory>
+#include <ostream>
+#include <vector>
 
 #include "utility.h"
 #include "Table.h"
@@ -23,6 +25,25 @@ enum struct Table0Axes {
   Y
 };
 
+std::ostream&
+operator<<(std::ostream& stream, const Table0Axes& ax) {
+  switch (ax) {
+  case Table0Axes::index:
+    stream << "Table0Axes::index";
+    break;
+  case Table0Axes::ROW:
+    stream << "Table0Axes::ROW";
+    break;
+  case Table0Axes::X:
+    stream << "Table0Axes::X";
+    break;
+  case Table0Axes::Y:
+    stream << "Table0Axes::Y";
+    break;
+  }
+  return stream;
+}
+
 #define TABLE0_NUM_X 4
 #define TABLE0_NUM_Y 3
 #define TABLE0_NUM_ROWS (TABLE0_NUM_X * TABLE0_NUM_Y)
@@ -30,8 +51,6 @@ unsigned table0_x[TABLE0_NUM_ROWS] {
                    0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3};
 unsigned table0_y[TABLE0_NUM_ROWS] {
                    0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2};
-unsigned table0_z[TABLE0_NUM_ROWS] {
-                   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
 ColumnT<Table0Axes>::Generator
 table0_col(const std::string& name) {
@@ -71,7 +90,7 @@ attach_table0_col(
   return runtime->attach_external_resource(context, task);
 }
 
-//#define TE(f) testing::TestEval([&](){ return f; })
+#define TE(f) testing::TestEval([&](){ return f; }, #f)
 
 void
 index_column_task_test_suite(
@@ -90,13 +109,15 @@ index_column_task_test_suite(
       context,
       runtime,
       "table0",
-      {table0_col("X"), table0_col("Y"), table0_col("Z")});
+      {"ROW"},
+      {table0_col("X"),
+       table0_col("Y")});
   auto col_x =
     attach_table0_col(table0.columnT("X").get(), table0_x, context, runtime);
   auto col_y =
     attach_table0_col(table0.columnT("Y").get(), table0_y, context, runtime);
-  IndexColumnTask icx(table0.columnT("X"));
-  IndexColumnTask icy(table0.columnT("Y"));
+  IndexColumnTask icx(table0.columnT("X"), static_cast<int>(Table0Axes::X));
+  IndexColumnTask icy(table0.columnT("Y"), static_cast<int>(Table0Axes::Y));
   Future fx = icx.dispatch(context, runtime);
   Future fy = icy.dispatch(context, runtime);
 
@@ -104,35 +125,33 @@ index_column_task_test_suite(
     fx.get_result<ColumnGenArgs>().operator()<Table0Axes>(context, runtime);
   recorder.assert_true(
     "IndexColumnTask X result has one axis",
-    cx->axesT().size() == 1);
+    TE(cx->axesT().size()) == 1u);
   recorder.expect_true(
-    "IndexColumnTask X result axis is 'index'",
-    cx->axesT()[0] == Table0Axes::index);
-  recorder.expect_true(
+    "IndexColumnTask X result axis is 'Table0Axes::X'",
+    TE(cx->axesT()[0]) == Table0Axes::X);
+  recorder.assert_true(
     "IndexColumnTask X result has one-dimensional IndexSpace",
-    cx->index_space().get_dim() == 1);
+    TE(cx->index_space().get_dim()) == 1);
   Domain xd = runtime->get_index_space_domain(cx->index_space());
   recorder.expect_true(
-    "IndexColumnTask X result IndexSpace has range [0,3]",
-    (xd.lo()[0] == 0)
-    && (xd.hi()[0] == TABLE0_NUM_X - 1));
+    "IndexColumnTask X result IndexSpace has expected range",
+    TE(xd.lo()[0] == 0) && (TE(xd.hi()[0]) == TABLE0_NUM_X - 1));
 
   auto cy =
     fy.get_result<ColumnGenArgs>().operator()<Table0Axes>(context, runtime);
   recorder.assert_true(
     "IndexColumnTask Y result has one axis",
-    cy->axesT().size() == 1);
+    TE(cy->axesT().size()) == 1u);
   recorder.expect_true(
-    "IndexColumnTask Y result axis is 'index'",
-    cy->axesT()[0] == Table0Axes::index);
-  recorder.expect_true(
+    "IndexColumnTask Y result axis is 'Table0Axes::Y'",
+    TE(cy->axesT()[0]) == Table0Axes::Y);
+  recorder.assert_true(
     "IndexColumnTask Y result has one-dimensional IndexSpace",
-    cy->index_space().get_dim() == 1);
+    TE(cy->index_space().get_dim()) == 1);
   Domain yd = runtime->get_index_space_domain(cy->index_space());
   recorder.expect_true(
-    "IndexColumnTask Y result IndexSpace has range [0,2]",
-    (yd.lo()[0] == 0)
-    && (yd.hi()[0] == TABLE0_NUM_Y - 1));
+    "IndexColumnTask Y result IndexSpace has expected range",
+    TE(yd.lo()[0] == 0) && (TE(yd.hi()[0]) == TABLE0_NUM_Y - 1));
 
   runtime->detach_external_resource(context, col_x);
   runtime->detach_external_resource(context, col_y);
@@ -145,16 +164,6 @@ main(int argc, char* argv[]) {
     testing::TestSuiteDriver::make<index_column_task_test_suite>(
       INDEX_COLUMN_TASK_TEST_SUITE,
       "index_column_task_test_suite");
-
-#if 0
-  {
-    TaskVariantRegistrar registrar(TEST_LOG_SUBTASK_ID, "subtask_suite");
-    registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
-    Runtime::preregister_task_variant<test_log_subtask>(
-      registrar,
-      "subtask_suite");
-  }
-#endif
 
   return driver.start(argc, argv);
 }
