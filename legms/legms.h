e695c13c11f8c1ffdf1680ef4d6578d75855835d
Martin Pokorny <mpokorny@nrao.edu>
2019-05-08 14:05:09 -0600

    Add files required by commit cc46330

---
+++ legms/legms.h
@@ -0,0 +1,14 @@
+#ifndef LEGMS_LEGMS_H_
+#define LEGMS_LEGMS_H_
+
+#include "legion.h"
+#include "legms_config.h"
+
+#endif // LEGMS_LEGMS_H_
+
+// Local Variables:
+// mode: c
+// c-basic-offset: 2
+// fill-column: 80
+// indent-tabs-mode: nil
+// End:
---
+++ legms/legms_config.h
@@ -0,0 +1,17 @@
+#ifndef LEGMS_LEGMS_CONFIG_H_
+#define LEGMS_LEGMS_CONFIG_H_
+
+#define LEGMS_MAX_DIM LEGION_MAX_DIM
+
+#ifndef LEGMS_MAX_STRING_SIZE
+# define LEGMS_MAX_STRING_SIZE 80
+#endif
+
+#endif // LEGMS_LEGMS_CONFIG_H_
+
+// Local Variables:
+// mode: c
+// c-basic-offset: 2
+// fill-column: 80
+// indent-tabs-mode: nil
+// End:
