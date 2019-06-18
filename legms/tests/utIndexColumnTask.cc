33727cad1b4ccbb35a7c00e74dbd83f56f7ee8c9
Martin Pokorny <mpokorny@nrao.edu>
2019-06-18 15:09:57 -0600

    Add check of values in utIndexColumnTask

--- legms/tests/utIndexColumnTask.cc
+++ legms/tests/utIndexColumnTask.cc
@@ -17,7 +17,6 @@ enum {
 };
 
 enum struct Table0Axes {
-  index = -1,
   ROW = 0,
   X,
   Y
@@ -47,9 +46,6 @@ hid_t TableT<Table0Axes>::m_h5_axes_datatype = h5_dt();
 std::ostream&
 operator<<(std::ostream& stream, const Table0Axes& ax) {
   switch (ax) {
-  case Table0Axes::index:
-    stream << "Table0Axes::index";
-    break;
   case Table0Axes::ROW:
     stream << "Table0Axes::ROW";
     break;
@@ -64,12 +60,20 @@ operator<<(std::ostream& stream, const Table0Axes& ax) {
 }
 
 #define TABLE0_NUM_X 4
+#define OX 22
 #define TABLE0_NUM_Y 3
+#define OY 30
 #define TABLE0_NUM_ROWS (TABLE0_NUM_X * TABLE0_NUM_Y)
 unsigned table0_x[TABLE0_NUM_ROWS] {
-                   0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3};
+                   OX + 0, OX + 0, OX + 0,
+                     OX + 1, OX + 1, OX + 1,
+                     OX + 2, OX + 2, OX + 2,
+                     OX + 3, OX + 3, OX + 3};
 unsigned table0_y[TABLE0_NUM_ROWS] {
-                   0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2};
+                   OY + 0, OY + 1, OY + 2,
+                     OY + 0, OY + 1, OY + 2,
+                     OY + 0, OY + 1, OY + 2,
+                     OY + 0, OY + 1, OY + 2};
 
 ColumnT<Table0Axes>::Generator
 table0_col(const std::string& name) {
@@ -155,6 +159,47 @@ index_column_task_test_suite(
   recorder.expect_true(
     "IndexColumnTask X result IndexSpace has expected range",
     TE(xd.lo()[0] == 0) && (TE(xd.hi()[0]) == TABLE0_NUM_X - 1));
+  recorder.expect_true(
+    "IndexColumnTask X result has expected values",
+    testing::TestEval(
+      [&context, runtime, &cx]() {
+        RegionRequirement
+          req(cx->logical_region(), READ_ONLY, EXCLUSIVE, cx->logical_region());
+        req.add_field(Column::value_fid);
+        PhysicalRegion pr = runtime->map_region(context, req);
+        const FieldAccessor<
+          READ_ONLY, unsigned, 1, coord_t,
+          AffineAccessor<unsigned, 1, coord_t>, true>
+          x(pr, Column::value_fid);
+        bool result =
+          x[0] == OX && x[1] == OX + 1 && x[2] == OX + 2 && x[3] == OX + 3;
+        runtime->unmap_region(context, pr);
+        return result;
+      }));
+  // recorder.expect_true(
+  //   "IndexColumnTask X result has expected index groups",
+  //   testing::TestEval(
+  //     [&context, runtime, &cx]() {
+  //       RegionRequirement
+  //         req(cx->logical_region(), READ_ONLY, EXCLUSIVE, cx->logical_region());
+  //       req.add_field(IndexColumnTask::rows_fid);
+  //       PhysicalRegion pr = runtime->map_region(context, req);
+  //       const FieldAccessor<
+  //         READ_ONLY, std::vector<DomainPoint>, 1, coord_t,
+  //         AffineAccessor<std::vector<DomainPoint>, 1, coord_t>, true>
+  //         x(pr, IndexColumnTask::rows_fid);
+  //       bool result =
+  //         (x[0] ==
+  //          std::vector<DomainPoint>{Point<1>(0), Point<1>(1), Point<1>(2)})
+  //         && (x[1] ==
+  //             std::vector<DomainPoint>{Point<1>(3), Point<1>(4), Point<1>(5)})
+  //         && (x[2] ==
+  //             std::vector<DomainPoint>{Point<1>(6), Point<1>(7), Point<1>(8)})
+  //         && (x[3] ==
+  //             std::vector<DomainPoint>{Point<1>(9), Point<1>(10), Point<1>(11)});
+  //       runtime->unmap_region(context, pr);
+  //       return result;
+  //     }));
 
   auto cy =
     fy.get_result<ColumnGenArgs>().operator()<Table0Axes>(context, runtime);
@@ -171,6 +216,47 @@ index_column_task_test_suite(
   recorder.expect_true(
     "IndexColumnTask Y result IndexSpace has expected range",
     TE(yd.lo()[0] == 0) && (TE(yd.hi()[0]) == TABLE0_NUM_Y - 1));
+  recorder.expect_true(
+    "IndexColumnTask Y result has expected values",
+    testing::TestEval(
+      [&context, runtime, &cy]() {
+        RegionRequirement
+          req(cy->logical_region(), READ_ONLY, EXCLUSIVE, cy->logical_region());
+        req.add_field(Column::value_fid);
+        PhysicalRegion pr = runtime->map_region(context, req);
+        const FieldAccessor<
+          READ_ONLY, unsigned, 1, coord_t,
+          AffineAccessor<unsigned, 1, coord_t>, true>
+          y(pr, Column::value_fid);
+        bool result = y[0] == OY && y[1] == OY + 1 && y[2] == OY + 2;
+        runtime->unmap_region(context, pr);
+        return result;
+      }));
+  // recorder.expect_true(
+  //   "IndexColumnTask Y result has expected index groups",
+  //   testing::TestEval(
+  //     [&context, runtime, &cy]() {
+  //       RegionRequirement
+  //         req(cy->logical_region(), READ_ONLY, EXCLUSIVE, cy->logical_region());
+  //       req.add_field(IndexColumnTask::rows_fid);
+  //       PhysicalRegion pr = runtime->map_region(context, req);
+  //       const FieldAccessor<
+  //         READ_ONLY, std::vector<DomainPoint>, 1, coord_t,
+  //         AffineAccessor<std::vector<DomainPoint>, 1, coord_t>, true>
+  //         y(pr, IndexColumnTask::rows_fid);
+  //       bool result =
+  //         (y[0] ==
+  //          std::vector<DomainPoint>{
+  //           Point<1>(0), Point<1>(3), Point<1>(6), Point<1>(9)})
+  //         && (y[1] ==
+  //             std::vector<DomainPoint>{
+  //               Point<1>(1), Point<1>(4), Point<1>(7), Point<1>(10)})
+  //         && (y[2] ==
+  //             std::vector<DomainPoint>{
+  //               Point<1>(2), Point<1>(5), Point<1>(8), Point<1>(11)});
+  //       runtime->unmap_region(context, pr);
+  //       return result;
+  //     }));
 
   runtime->detach_external_resource(context, col_x);
   runtime->detach_external_resource(context, col_y);
