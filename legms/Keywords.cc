b79a7a47537f481b8170a453c91ab3af28f8db08
Martin Pokorny <mpokorny@nrao.edu>
2019-08-16 11:43:24 -0600

    Fix Keywords

---
+++ legms/Keywords.cc
@@ -0,0 +1,121 @@
+#include "Keywords.h"
+
+using namespace legms;
+using namespace Legion;
+
+Keywords::Keywords(pair<LogicalRegion> regions)
+  : type_tags(regions.type_tags)
+  , values(regions.values) {
+}
+
+std::vector<std::string>
+Keywords::keys(Runtime* rt) const {
+
+  auto fs = type_tags.get_field_space();
+  std::unordered_set<FieldID> fids;
+  rt->get_field_space_fields(fs, fids);
+  std::vector<std::string> result(fids.size());
+  for (auto& fid : fids) {
+    const char* fname;
+    rt->retrieve_name(fs, fid, fname);
+    result[fid] = fname;
+  }
+  return result;
+}
+
+std::optional<FieldID>
+Keywords::find_keyword(Runtime* rt, const std::string& name) const {
+
+  auto fs = type_tags.get_field_space();
+  std::vector<FieldID> fids;
+  rt->get_field_space_fields(fs, fids);
+  auto f =
+    std::find_if(
+      fids.begin(),
+      fids.end(),
+      [&name, &fs, rt](auto fid) {
+        const char* fname;
+        rt->retrieve_name(fs, fid, fname);
+        return name == fname;
+      });
+  return ((f != fids.end()) ? (*f - 1) : std::nullopt);
+}
+
+std::vector<legms::TypeTag>
+Keywords::value_types(
+  Context ctx,
+  Runtime* rt,
+  const std::vector<FieldID>& fids) const {
+
+  // allow runtime to catch erroneous FieldIDs
+  RegionRequirement req(type_tags, READ_ONLY, EXCLUSIVE, type_tags);
+  for (auto& fid : fids)
+    req.add_field(fid);
+  auto pr = rt->map_region(ctx, req);
+  std::vector<legms::TypeTag> result;
+  result.reserve(fids.size());
+  for (auto& fid : fids)
+    result.push_pack(value_type(pr, fid));
+  rt->unmap_region(ctx, pr);
+  return result;
+}
+
+Keywords
+Keywords::create(Context ctx, Runtime* rt, const kw_desc_t& kws) {
+
+  LogicalRegion tts, vals;
+  if (kws.size() > 0) {
+    auto is = rt->create_index_space(ctx, Rect<1>(0, 0));
+    auto tt_fs = rt->create_field_space(ctx);
+    auto tt_fa = rt->create_field_allocator(ctx, tt_fs);
+    auto val_fs = rt->create_field_space(ctx);
+    auto val_fa = rt->create_field_allocator(ctx, val_fs);
+    for (size_t i = 0; i < kws.size(); ++i) {
+      auto& [nm, dt] = kws[i];
+      tt_fa.allocate_field(sizeof(legms::TypeTag), i);
+      rt->attach_name(tt_fs, i, nm.c_str());
+      add_field(dt, val_fa, i);
+    }
+    tts = rt->create_logical_region(ctx, is, tt_fs);
+    vals = rt->create_logical_region(ctx, is, val_fs);
+    RegionRequirement req(tts, WRITE_ONLY, EXCLUSIVE, tts);
+    for (size_t i = 0; i < kws.size(); ++i)
+      req.add_field(i);
+    auto pr = rt->map_region(ctx, req);
+    for (size_t i = 0; i < kws.size(), ++i) {
+      const TypeTagAccessor<WRITE_ONLY> dt(pr, i);
+      dt[0] = std::get<1>(kws[i]);
+    }
+    rt->unmap_region(ctx, pr);
+  }
+  return Keywords(tts, vals);
+}
+
+void
+Keywords::destroy(Context ctx, Runtime* rt) {
+  if (type_tags != LogicalRegion::NO_REGION) {
+    assert(values != LogicalRegion::NO_REGION);
+    rt->destroy_field_space(ctx, type_tags.get_field_space());
+    rt->destroy_field_space(ctx, values.get_field_space());
+    // type_tags and values share one IndexSpace
+    rt->destroy_index_space(ctx, type_tags.get_index_space());
+    rt->destroy_logical_region(ctx, type_tags);
+    rt->destroy_logical_region(ctx, values);
+    type_tags = LogicalRegion::NO_REGION;
+    values = LogicalRegion::NO_REGION;
+  }
+}
+
+static legms::TypeTag
+Keywords::value_type(const PhysicalRegion& tt, FieldID fid) {
+
+  const TypeTagAccessor<READ_ONLY> dt(tt, fid);
+  return dt[0];
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// fill-column: 80
+// indent-tabs-mode: nil
+// End:
--- legms/Keywords.h
+++ legms/Keywords.h
@@ -36,10 +36,10 @@ struct LEGMS_API Keywords {
   using TypeTagAccessor =
     FieldAccessor<
     MODE,
-    TypeTag,
+    legms::TypeTag,
     1,
     coord_t,
-    Legion::AffineAccessor<TypeTag, 1, coord_t>,
+    Legion::AffineAccessor<legms::TypeTag, 1, coord_t>,
     CHECK_BOUNDS>;
 
   template <legion_privilege_mode_t MODE, typename FT, bool CHECK_BOUNDS=false>
@@ -52,67 +52,32 @@ struct LEGMS_API Keywords {
     Legion::AffineAccessor<FT, 1, coord_t>,
     CHECK_BOUNDS>;
 
-  Keywords(pair<Legion::LogicalRegion> regions)
-    : type_tags(regions.type_tags)
-    , values(regions.values) {}
+  typedef std::vector<std::tuple<std::string, legms::TypeTag>> kw_desc_t;
+
+  Keywords(pair<Legion::LogicalRegion> regions);
 
   std::vector<std::string>
-  keys() const {
-
-    auto fs = type_tags.get_field_space();
-    std::unordered_map<Legion::FieldID> fids;
-    rt->get_field_space_fields(fs, fids);
-    std::vector<std::string> result(fids.size());
-    std::for_each(
-      fids.begin(),
-      fids.end(),
-      [&fs, rt](auto fid) {
-        const char* fname;
-        rt->retrieve_name(fs, fid, fname);
-        result[fid] = fname;
-      });
-    return result;
-  }
+  keys(Legion::Runtime* rt) const;
 
   std::optional<Legion::FieldID>
-  find_keyword(Legion::Runtime* rt, const std::string& name) const {
+  find_keyword(Legion::Runtime* rt, const std::string& name) const;
 
-    auto fs = type_tags.get_field_space();
-    std::vector<Legion::FieldID> fids;
-    rt->get_field_space_fields(fs, fids);
-    auto f =
-      std::find_if(
-        fids.begin(),
-        fids.end(),
-        [&name, &fs, rt](auto fid) {
-          const char* fname;
-          rt->retrieve_name(fs, fid, fname);
-          return name == fname;
-        });
-    return ((f != fids.end()) ? (*f - 1) : std::nullopt);
-  }
-
-  TypeTag
-  value_type(
+  std::vector<legms::TypeTag>
+  value_types(
     Legion::Context ctx,
     Legion::Runtime* rt,
-    Legion::FieldID fid) const {
-
-    RegionRequirement req(type_tags, READ_ONLY, EXCLUSIVE, type_tags);
-    auto pr = rt->map_region(ctx, req);
-    auto result = value_type(pr, fid);
-    rt->unmap_region(ctx, pr);
-    return result;
-  }
+    const std::vector<Legion::FieldID>& fids) const;
 
   template <legion_privilege_mode_t MODE, template <typename> C>
   pair<Legion::RegionRequirement>
   requirements(const C<FieldID>& fids) const {
+
+    // allow runtime to catch erroneous FieldIDs
     RegionRequirement tt(type_tags, READ_ONLY, EXCLUSIVE, type_tags);
     RegionRequirement v(values, MODE, EXCLUSIVE, values);
     std::for_each(
-      fids.begin(),
-      fids.end(),
+      std::begin(fids),
+      std::end(fids),
       [&tt, &v](auto fid) {
         tt.add_field(fid);
         v.add_field(fid);
@@ -123,69 +88,45 @@ struct LEGMS_API Keywords {
   template <typename T>
   bool
   write(
-    Legion::FieldID fid,
-    const T& val,
     Legion::Context ctx,
-    Legion::Runtime* rt) const {
+    Legion::Runtime* rt,
+    Legion::FieldID fid,
+    const T& t) const {
 
+    bool result = false;
     auto reqs = requirements<WRITE_ONLY>(std::vector<FieldID>{fid});
-    auto prs = reqs.map([&ctx, rt](auto& r){ return rt->map_region(ctx, r); });
-    bool result = write(prs, fid, val);
-    prs.map([&ctx, rt](auto& p) { rt->unmap_region(ctx, p); });
+    if (reqs) {
+      auto prs =
+        reqs.value().map([&ctx, rt](auto& r){ return rt->map_region(ctx, r); });
+      result = write(prs, fid, t);
+      prs.map([&ctx, rt](auto& p) { rt->unmap_region(ctx, p); });
+    }
     return result;
   }
 
   template <typename T>
   std::optional<T>
-  read(Legion::FieldID fid, Legion::Context ctx, Legion::Runtime* rt) const {
+  read(Legion::Context ctx, Legion::Runtime* rt, Legion::FieldID fid) const {
 
+    bool result = false;
     auto reqs = requirements<READ_ONLY>(std::vector<FieldID>{fid});
-    auto prs = reqs.map([&ctx, rt](auto& r){ return rt->map_region(ctx, r); });
-    auto result = read(prs, fid);
-    prs.map([&ctx, rt](auto& p) { rt->unmap_region(ctx, p); });
+    if (reqs) {
+      auto prs =
+        reqs.value().map([&ctx, rt](auto& r){ return rt->map_region(ctx, r); });
+      result = read(prs, fid);
+      prs.map([&ctx, rt](auto& p) { rt->unmap_region(ctx, p); });
+    }
     return result;
   }
 
   static Keywords
-  make(
-    Legion::Context ctx,
-    Legion::Runtime* rt,
-    const std::vector<std::tuple<std::string, TypeTag>>& kws) {
+  create(Legion::Context ctx, Legion::Runtime* rt, const kw_desc_t& kws);
 
-    LogicalRegion tts, vals;
-    if (kws.size() > 0) {
-      auto is = rt->create_index_space(ctx, Legion::Rect<1>(0, 0));
-      auto tt_fs = rt->create_field_space(ctx);
-      auto tt_fa = rt->create_field_allocator(ctx, tt_fs);
-      auto val_fs = rt->create_field_space(ctx);
-      auto val_fa = rt->create_field_allocator(ctx, val_fs);
-      for (size_t i = 0; i < kws.size(); ++i) {
-        auto& [nm, dt] = kws[i];
-        tt_fa.allocate_field(sizeof(TypeTag), i);
-        rt->attach_name(tt_fs, i, nm.c_str());
-        add_field(dt, val_fa, i);
-      }
-      tts = rt->create_logical_region(ctx, is, tt_fs);
-      vals = rt->create_logical_region(ctx, is, val_fs);
-      RegionRequirement req(tts, WRITE_ONLY, EXCLUSIVE, tts);
-      for (size_t i = 0; i < kws.size(); ++i)
-        req.add_field(i);
-      auto pr = rt->map_region(ctx, req);
-      for (size_t i = 0; i < kws.size(), ++i) {
-        const TypeTagAccessor<WRITE_ONLY> dt(pr, i);
-        dt[0] = std::get<1>(kws[i]);
-      }
-      rt->unmap_region(ctx, pr);
-    }
-    return Keywords(tts, vals);
-  }
+  void
+  destroy(Legion::Context ctx, Legion::Runtime* rt);
 
-  static TypeTag
-  value_type(const Legion::PhysicalRegion& tt, Legion::FieldID fid) {
-
-    const TypeTagAccessor<READ_ONLY> dt(tt, fid);
-    return dt[0];
-  }
+  static legms::TypeTag
+  value_type(const Legion::PhysicalRegion& tt, Legion::FieldID fid);
 
   template <int MODE, typename T>
   static bool
