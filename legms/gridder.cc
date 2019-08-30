5b3b8a139f3cbd7830391b968e3be6fe974c8437
Martin Pokorny <mpokorny@nrao.edu>
2019-08-30 17:09:43 -0600

    Compute cf cache index space

--- legms/gridder.cc
+++ legms/gridder.cc
@@ -25,6 +25,12 @@
 #include "Table.h"
 #include "Column.h"
 
+#define DEFAULT_PA_STEP_STR "360.0"
+#define DEFAULT_W_PROJ_PLANES_STR "1"
+
+#define AUTO_W_PROJ_PLANES_VALUE -1
+#define INVALID_W_PROJ_PLANES_VALUE -2
+
 using namespace legms;
 using namespace Legion;
 
@@ -85,16 +91,29 @@ const std::array<const char*, 0> TableColumns<T>::column_names;
 template <MSTables T>
 const char* TableColumns<T>::table_name = "";
 
+template <typename T>
+struct WithColumnLookup {
+  static unsigned
+  column_offset(const std::string& name) {
+    auto p = std::find(T::column_names.begin(), T::column_names.end(), name);
+    if (p == T::column_names.end())
+      throw std::domain_error(
+        "column '" + name + "' not present in table '" + T::table_name + "'");
+    return std::distance(T::column_names.begin(), p);
+  }
+};
+
 template <>
-struct TableColumns<MS_ANTENNA> {
-  enum {
+struct TableColumns<MS_ANTENNA>
+  : public WithColumnLookup<TableColumns<MS_ANTENNA>> {
+  typedef enum {
     NAME,
     STATION,
     TYPE,
     MOUNT,
     DISH_DIAMETER,
     NUM_COLUMNS
-  };
+  } col;
   static constexpr const std::array<const char*,NUM_COLUMNS> column_names = {
     "NAME",
     "STATION",
@@ -103,16 +122,56 @@ struct TableColumns<MS_ANTENNA> {
     "DISH_DIAMETER"
   };
   static constexpr const char* table_name = MSTable<MS_ANTENNA>::name;
-
-  static unsigned
-  column_offset(const std::string& name) {
-    auto p = std::find(column_names.begin(), column_names.end(), name);
-    if (p == column_names.end())
-      throw std::domain_error(
-        "column '" + name + "' not present in table '" + table_name + "'");
-    return std::distance(column_names.begin(), p);
-  }
 };
+
+template <>
+struct TableColumns<MS_DATA_DESCRIPTION>
+  : public WithColumnLookup<TableColumns<MS_DATA_DESCRIPTION>> {
+  typedef enum {
+    SPECTRAL_WINDOW_ID,
+    POLARIZATION_ID,
+    NUM_COLUMNS
+  } col;
+  static constexpr const std::array<const char*,NUM_COLUMNS> column_names = {
+    "SPECTRAL_WINDOW_ID",
+    "POLARIZATION_ID"
+  };
+  static constexpr const char* table_name = MSTable<MS_DATA_DESCRIPTION>::name;
+};
+
+template <>
+struct TableColumns<MS_POLARIZATION>
+  : public WithColumnLookup<TableColumns<MS_POLARIZATION>> {
+  typedef enum {
+    NUM_CORR,
+    NUM_COLUMNS
+  } col;
+  static constexpr const std::array<const char*,NUM_COLUMNS> column_names = {
+    "NUM_CORR",
+  };
+  static constexpr const char* table_name = MSTable<MS_POLARIZATION>::name;
+};
+
+template <>
+struct TableColumns<MS_SPECTRAL_WINDOW>
+  : public WithColumnLookup<TableColumns<MS_SPECTRAL_WINDOW>> {
+  typedef enum {
+    NUM_CHAN,
+    REF_FREQUENCY,
+    CHAN_FREQ,
+    CHAN_WIDTH,
+    NUM_COLUMNS
+  } col;
+  static constexpr const std::array<const char*,NUM_COLUMNS> column_names = {
+    "NUM_CHAN",
+    "REF_FREQUENCY",
+    "CHAN_FREQ",
+    "CHAN_WIDTH"
+  };
+  static constexpr const char* table_name = MSTable<MS_SPECTRAL_WINDOW>::name;
+};
+
+#define COLUMN_NAME(T, C) TableColumns<T>::column_names[TableColumns<T>::C]
 
 Table
 init_table(
@@ -179,7 +238,7 @@ public:
         RegionRequirement
           vreq(col.values_lr, READ_ONLY, EXCLUSIVE, col.values_lr);
         vreq.add_field(Column::VALUE_FID);
-        requirements.push_back(vreq);      
+        requirements.push_back(vreq);
       }
       rt->unmap_region(ctx, cols);
     }
@@ -248,6 +307,7 @@ public:
 
   static FieldID TASK_ID;
   static const constexpr char* TASK_NAME = "UnitaryClassifyAntennasTask";
+  static const unsigned num_classes = 1;
 
   UnitaryClassifyAntennasTask(const Table& table)
     : ClassifyAntennasTask(table) {
@@ -274,7 +334,133 @@ public:
   }
 };
 
+struct PAValues {
+  static const FieldID PA_ORIGIN_FID = 0;
+  static const FieldID PA_STEP_FID = 1;
+  static const FieldID PA_NUM_STEP_FID = 2;
+
+  LogicalRegion parameters;
+
+  PAValues(
+    Context ctx,
+    Runtime* rt,
+    const std::unordered_map<MSTables,Table>&,
+    float pa_step,
+    float pa_origin = 0.0f) {
+
+    IndexSpace is = rt->create_index_space(ctx, Rect<1>(0, 0));
+    FieldSpace fs = rt->create_field_space(ctx);
+    FieldAllocator fa = rt->create_field_allocator(ctx, fs);
+    fa.allocate_field(sizeof(float), PA_ORIGIN_FID);
+    fa.allocate_field(sizeof(float), PA_STEP_FID);
+    fa.allocate_field(sizeof(unsigned long), PA_NUM_STEP_FID);
+    parameters = rt->create_logical_region(ctx, is, fs);
+
+    RegionRequirement req(parameters, WRITE_ONLY, EXCLUSIVE, parameters);
+    req.add_field(PA_ORIGIN_FID);
+    req.add_field(PA_STEP_FID);
+    req.add_field(PA_NUM_STEP_FID);
+    auto pr = rt->map_region(ctx, req);
+    const WOAccessor<float, 1> origin(pr, PA_ORIGIN_FID);
+    const WOAccessor<float, 1> step(pr, PA_STEP_FID);
+    const WOAccessor<unsigned long, 1> num_step(pr, PA_STEP_FID);
+    origin[0] = pa_origin;
+    step[0] = pa_step;
+    num_step[0] = std::lrint(std::ceil(360.0f / pa_step)) - 1;
+    rt->unmap_region(ctx, pr);
+  }
+
+  unsigned long
+  num_steps(Context ctx, Runtime* rt) const {
+    RegionRequirement req(parameters, READ_ONLY, EXCLUSIVE, parameters);
+    req.add_field(PA_NUM_STEP_FID);
+    auto pr = rt->map_region(ctx, req);
+    auto result = num_steps(pr);
+    rt->unmap_region(ctx, pr);
+    return result;
+  }
+
+  static unsigned long
+  num_steps(PhysicalRegion region) {
+    const ROAccessor<unsigned long, 1> num_step(region, PA_NUM_STEP_FID);
+    return num_step[0];
+  }
+
+  std::optional<std::tuple<float, float>>
+  pa(Context ctx, Runtime* rt, unsigned long i) const {
+    RegionRequirement req(parameters, READ_ONLY, EXCLUSIVE, parameters);
+    req.add_field(PA_ORIGIN_FID);
+    req.add_field(PA_STEP_FID);
+    req.add_field(PA_NUM_STEP_FID);
+    auto pr = rt->map_region(ctx, req);
+    auto result = pa(pr, i);
+    rt->unmap_region(ctx, pr);
+    return result;
+  }
+
+  static std::optional<std::tuple<float, float>>
+  pa(PhysicalRegion region, unsigned long i) {
+    const ROAccessor<float, 1> origin(region, PA_ORIGIN_FID);
+    const ROAccessor<float, 1> step(region, PA_STEP_FID);
+    const ROAccessor<unsigned long, 1> num_step(region, PA_NUM_STEP_FID);
+    if (i >= num_step[0])
+      return std::nullopt;
+    float lo = i * step[0];
+    float width = (i == num_step[0] - 1) ? (360.0f - lo) : step[0];
+    return std::make_tuple(origin[0] + lo, width);
+  }
+
+  unsigned long
+  find(Context ctx, Runtime* rt, float pa) const {
+    RegionRequirement req(parameters, READ_ONLY, EXCLUSIVE, parameters);
+    req.add_field(PA_ORIGIN_FID);
+    req.add_field(PA_STEP_FID);
+    auto pr = rt->map_region(ctx, req);
+    auto result = find(pr, pa);
+    rt->unmap_region(ctx, pr);
+    return result;
+  }
+
+  static unsigned long
+  find(PhysicalRegion region, float pa) {
+    const ROAccessor<float, 1> origin(region, PA_ORIGIN_FID);
+    const ROAccessor<float, 1> step(region, PA_STEP_FID);
+    float a = pa - origin[0];
+    if (a < 0.0f)
+      a += std::ceil(std::abs(a) / 360.0f) * 360.0f;
+    return std::lrint(std::floor(a / step[0]));
+  }
+
+  void
+  destroy(Context ctx, Runtime* rt) {
+    rt->destroy_field_space(ctx, parameters.get_field_space());
+    rt->destroy_index_space(ctx, parameters.get_index_space());
+    rt->destroy_logical_region(ctx, parameters);
+  }
+};
+
 FieldID UnitaryClassifyAntennasTask::TASK_ID = 0;
+
+typedef enum {VALUE_ARGS, STRING_ARGS} gridder_args_t;
+
+template <typename T, gridder_args_t G>
+struct GridderArgType {
+};
+template <typename T>
+struct GridderArgType<T, VALUE_ARGS> {
+  typedef T type;
+};
+template <typename T>
+struct GridderArgType<T, STRING_ARGS> {
+  typedef std::string type;
+};
+
+template <gridder_args_t G>
+struct GridderArgs {
+  typename GridderArgType<LEGMS_FS::path, G>::type h5;
+  typename GridderArgType<float, G>::type pa_step;
+  typename GridderArgType<int, G>::type w_proj_planes;
+};
 
 class TopLevelTask {
 public:
@@ -283,24 +469,140 @@ public:
   static constexpr const char* TASK_NAME = "TopLevelTask";
 
   static void
-  get_args(const Legion::InputArgs& args, LEGMS_FS::path& ms) {
-    ms.clear();
+  get_args(
+    const Legion::InputArgs& args,
+    GridderArgs<STRING_ARGS>& gridder_args) {
+
     for (int i = 1; i < args.argc; ++i) {
-      if (std::string(args.argv[i]) == "--ms")
-        ms = args.argv[++i];
+      std::string ai = args.argv[i];
+      if (ai == "--ms")
+        gridder_args.h5 = args.argv[++i];
+      else if (ai == "--pastep")
+        gridder_args.pa_step = args.argv[++i];
+      else if (ai == "--wprojplanes")
+        gridder_args.w_proj_planes = args.argv[++i];
     }
   }
 
   static std::optional<std::string>
-  args_error(const LEGMS_FS::path& h5) {
-    auto h5_abs = LEGMS_FS::canonical(h5);
-    if (LEGMS_FS::exists(h5_abs) && LEGMS_FS::is_regular_file(h5_abs)) {
-      return std::nullopt;
-    } else {
-      std::ostringstream oss;
-      oss << "Path '" << h5 << "' does not name a regular file";
-      return oss.str();
+  args_error(
+    const GridderArgs<STRING_ARGS>& str_args,
+    const GridderArgs<VALUE_ARGS>& val_args) {
+
+    std::ostringstream errs;
+    auto h5_abs = LEGMS_FS::canonical(val_args.h5);
+    if (!LEGMS_FS::exists(h5_abs) || !LEGMS_FS::is_regular_file(h5_abs))
+      errs << "Path '" << str_args.h5
+           << "' does not name a regular file" << std::endl;
+    switch (std::fpclassify(val_args.pa_step)) {
+    case FP_NORMAL:
+      break;
+    case FP_ZERO:
+    case FP_SUBNORMAL:
+      errs << "--pastep value '" << str_args.pa_step
+           << "' is too small" << std::endl;
+      break;
+    default:
+      errs << "--pastep value '" << str_args.pa_step
+           << "' is invalid" << std::endl;
+      break;
     }
+    if (val_args.w_proj_planes <= INVALID_W_PROJ_PLANES_VALUE)
+      errs << "--wprojplanes value '" << str_args.w_proj_planes
+           << "' is less than the minimum valid value of "
+           << INVALID_W_PROJ_PLANES_VALUE + 1 << std::endl;
+    else if (val_args.w_proj_planes == AUTO_W_PROJ_PLANES_VALUE)
+      errs << "Automatic computation of the number of W-projection "
+           << "planes is unimplemented" << std::endl;
+    if (errs.str().size() > 0)
+      return errs.str();
+    return std::nullopt;
+  }
+
+  static IndexSpace
+  compute_cf_cache_index_space(
+    Context ctx,
+    Runtime* rt,
+    unsigned num_antenna_classes,
+    const GridderArgs<VALUE_ARGS>& gridder_args,
+    const PAValues& pa_values,
+    const Table& dd_table,
+    const Column& num_chan_col,
+    const Column& num_corr_col) {
+
+    auto num_pa_steps = pa_values.num_steps(ctx, rt);
+
+    coord_t num_dd;
+    PhysicalRegion spw_id_pr;
+    {
+      const Column& col =
+        dd_table.column(
+          ctx,
+          rt,
+          COLUMN_NAME(MS_DATA_DESCRIPTION, SPECTRAL_WINDOW_ID));
+      Rect<1> rows(
+        rt->get_index_space_domain(col.values_lr.get_index_space()));
+      num_dd = rows.hi[0] + 1;
+      RegionRequirement req(col.values_lr, READ_ONLY, EXCLUSIVE, col.values_lr);
+      req.add_field(Column::VALUE_FID);
+      spw_id_pr = rt->map_region(ctx, req);
+    }
+    const ROAccessor<int, 1> spw_ids(spw_id_pr, Column::VALUE_FID);    
+
+    PhysicalRegion pol_id_pr;
+    {
+      const Column& col =
+        dd_table.column(
+          ctx,
+          rt,
+          COLUMN_NAME(MS_DATA_DESCRIPTION, POLARIZATION_ID));
+      RegionRequirement req(col.values_lr, READ_ONLY, EXCLUSIVE, col.values_lr);
+      req.add_field(Column::VALUE_FID);
+      pol_id_pr = rt->map_region(ctx, req);
+    }
+    const ROAccessor<int, 1> pol_ids(pol_id_pr, Column::VALUE_FID);
+
+    PhysicalRegion num_chan_pr;
+    {
+      RegionRequirement
+        req(
+          num_chan_col.values_lr,
+          READ_ONLY,
+          EXCLUSIVE,
+          num_chan_col.values_lr);
+      num_chan_pr = rt->map_region(ctx, req);
+    }
+    const ROAccessor<int, 1> num_chan(num_chan_pr, Column::VALUE_FID);
+
+    PhysicalRegion num_corr_pr;
+    {
+      RegionRequirement
+        req(
+          num_corr_col.values_lr,
+          READ_ONLY,
+          EXCLUSIVE,
+          num_corr_col.values_lr);
+      num_corr_pr = rt->map_region(ctx, req);
+    }
+    const ROAccessor<int, 1> num_corr(num_corr_pr, Column::VALUE_FID);
+
+    std::vector<IndexSpace> is_pieces;
+    for (coord_t ant0 = 0; ant0 < num_antenna_classes; ++ant0)
+      for (coord_t ant1 = ant0; ant1 < num_antenna_classes; ++ant1)
+        for (coord_t pa = 0; pa < num_pa_steps; ++pa)
+          for (coord_t dd = 0; dd < num_dd; ++dd) {
+            coord_t lo[7]{ant0, ant1, pa, dd, 0, 0, 0};
+            coord_t hi[7]{ant0, ant1, pa, dd, num_chan[spw_ids[dd]] - 1,
+                gridder_args.w_proj_planes - 1, num_corr[pol_ids[dd]] - 1};
+            is_pieces.push_back(
+              rt->create_index_space(ctx, Rect<7>(Point<7>(lo), Point<7>(hi))));
+          }
+    IndexSpace result = rt->union_index_spaces(ctx, is_pieces);
+    rt->unmap_region(ctx, pol_id_pr);
+    rt->unmap_region(ctx, spw_id_pr);
+    rt->unmap_region(ctx, num_chan_pr);
+    rt->unmap_region(ctx, num_corr_pr);
+    return result;
   }
 
   static void
@@ -310,39 +612,125 @@ public:
     Legion::Context ctx,
     Legion::Runtime* rt) {
 
-    const Legion::InputArgs& args = Legion::Runtime::get_input_args();
-    LEGMS_FS::path h5;
-    get_args(args, h5);
+    legms::register_tasks(ctx, rt);
+
+    GridderArgs<VALUE_ARGS> gridder_args;
     {
-      auto errstr = args_error(h5);
+      const Legion::InputArgs& input_args = Legion::Runtime::get_input_args();
+      GridderArgs<STRING_ARGS> str_args;
+      str_args.pa_step = DEFAULT_PA_STEP_STR;
+      str_args.w_proj_planes = DEFAULT_W_PROJ_PLANES_STR;
+      get_args(input_args, str_args);
+      gridder_args.h5 = str_args.h5;
+      try {
+        std::size_t pos;
+        gridder_args.pa_step = std::stof(str_args.pa_step, &pos);
+        if (pos != str_args.pa_step.size())
+          gridder_args.pa_step = NAN;
+      } catch (const std::invalid_argument&) {
+        gridder_args.pa_step = NAN;
+      } catch (const std::out_of_range&) {
+        gridder_args.pa_step = NAN;
+      }
+      try {
+        std::size_t pos;
+        gridder_args.w_proj_planes = std::stoi(str_args.w_proj_planes, &pos);
+        if (pos != str_args.w_proj_planes.size())
+          gridder_args.w_proj_planes = INVALID_W_PROJ_PLANES_VALUE;
+        if (gridder_args.w_proj_planes == 0)
+          gridder_args.w_proj_planes = 1;
+      } catch (const std::invalid_argument&) {
+        gridder_args.pa_step = INVALID_W_PROJ_PLANES_VALUE;
+      } catch (const std::out_of_range&) {
+        gridder_args.pa_step = INVALID_W_PROJ_PLANES_VALUE;
+      }
+      auto errstr = args_error(str_args, gridder_args);
       if (errstr) {
         std::cerr << errstr.value() << std::endl;
         return;
       }
     }
+    gridder_args.pa_step = std::fmod(std::abs(gridder_args.pa_step), 360.f);
+
     const std::string ms_root = "/";
-
-    legms::register_tasks(ctx, rt);
-
-    MSTables mstables[] = {MS_ANTENNA};
+    MSTables mstables[] = {
+      MS_ANTENNA,
+      MS_DATA_DESCRIPTION,
+      MS_POLARIZATION,
+      MS_SPECTRAL_WINDOW
+    };
     std::unordered_map<MSTables,Table> tables;
     for (auto& mst : mstables)
-      tables[mst] = init_table(ctx, rt, h5, ms_root, mst);
+      tables[mst] = init_table(ctx, rt, gridder_args.h5, ms_root, mst);
 
     LogicalRegion antenna_classes;
     {
-      Table& antenna_table = tables[MS_ANTENNA];
-      antenna_table.with_columns_attached(
-        ctx,
-        rt,
-        h5,
-        ms_root,
-        [&antenna_classes, &antenna_table](Context ctx, Runtime* rt) {
-          UnitaryClassifyAntennasTask task(antenna_table);
-          antenna_classes = task.dispatch(ctx, rt);
-        });
+      tables[MS_ANTENNA]
+        .with_columns_attached(
+          ctx,
+          rt,
+          gridder_args.h5,
+          ms_root,
+          [&antenna_classes]
+          (Context ctx, Runtime* rt, const Table& tb) {
+            UnitaryClassifyAntennasTask task(tb);
+            antenna_classes = task.dispatch(ctx, rt);
+          });
     }
+    rt->attach_name(antenna_classes, "antenna_classes");
 
+    PAValues pa_values(ctx, rt, tables, gridder_args.pa_step);
+    rt->attach_name(pa_values.parameters, "pa_values");
+
+    // IndexSpace cf_cache_is;    
+    // tables[MS_DATA_DESCRIPTION]
+    //   .with_columns_attached(
+    //     ctx,
+    //     rt,
+    //     gridder_args.h5,
+    //     ms_root,
+    //     [&tables, &gridder_args, &pa_values, &ms_root, &cf_cache_is]
+    //     (Context ctx, Runtime* rt, const Table& dd) {
+    //       tables.[MS_SPECTRAL_WINDOW]
+    //         .column(
+    //           ctx,
+    //           rt,
+    //           COLUMN_NAME(MS_SPECTRAL_WINDOW, NUM_CHAN))
+    //         .with_attached(
+    //           ctx,
+    //           rt,
+    //           gridder_args.h5,
+    //           ms_root + TableColumns<MS_SPECTRAL_WINDOW>::table_name,
+    //           [&tables, &gridder_args, &pa_values, &ms_root, &dd, &cf_cache_is]
+    //           (Context ctx, Runtime* rt, const Column& num_chan) {
+    //             tables[MS_POLARIZATION]
+    //               .column(
+    //                 ctx,
+    //                 rt,
+    //                 COLUMN_NAME(MS_POLARIZATION, NUM_CORR))
+    //               .with_attached(
+    //                 ctx,
+    //                 rt,
+    //                 gridder_args.h5,
+    //                 ms_root + TableColumns<MS_POLARIZATION>::table_name,
+    //                 [&gridder_args, &pa_values, &num_chan, &dd, &cf_cache_is]
+    //                 (Context ctx, Runtime* rt, const Column& num_corr) {
+    //                   cf_cache_is =
+    //                     compute_cf_cache_index_space(
+    //                       ctx,
+    //                       rt,
+    //                       UnitaryClassifyAntennasTask::num_classes,
+    //                       gridder_args,
+    //                       pa_values,
+    //                       dd,
+    //                       num_chan,
+    //                       num_corr);
+    //                 })
+    //               });        
+    //     });
+
+    // rt->destroy_index_space(cf_cache_is);
+    pa_values.destroy(ctx, rt);
     rt->destroy_field_space(ctx, antenna_classes.get_field_space());
     rt->destroy_logical_region(ctx, antenna_classes);
   }
@@ -355,6 +743,7 @@ public:
     Legion::Runtime::preregister_task_variant<base_impl>(registrar, TASK_NAME);
     Legion::Runtime::set_top_level_task_id(TASK_ID);
   }
+
 };
 
 int
