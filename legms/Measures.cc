45c6f60fc8bec5803a008d0aff57602198c09e43
Martin Pokorny <mpokorny@nrao.edu>
2019-09-19 11:05:55 -0600

    Initial version of Measures module

---
+++ legms/Measures.cc
@@ -0,0 +1,545 @@
+#include "Measures.h"
+
+#ifdef LEGMS_USE_CASACORE
+
+#include "IndexTree.h"
+#include "tree_index_space.h"
+#include "utility.h"
+
+#include <casacore/measures/Measures/MBaseline.h>
+#include <casacore/measures/Measures/MDirection.h>
+#include <casacore/measures/Measures/MDoppler.h>
+#include <casacore/measures/Measures/MEarthMagnetic.h>
+#include <casacore/measures/Measures/MEpoch.h>
+#include <casacore/measures/Measures/MFrequency.h>
+#include <casacore/measures/Measures/MPosition.h>
+#include <casacore/measures/Measures/MRadialVelocity.h>
+#include <casacore/measures/Measures/Muvw.h>
+
+#include <cassert>
+#include <stack>
+#include <unordered_map>
+
+using namespace legms;
+using namespace Legion;
+
+template <MeasureRegion::MClass k>
+struct MClass {
+  // typedef casacore::... type;
+  // std::string name;
+};
+template <>
+struct MClass<MeasureRegion::MClass::M_BASELINE> {
+  typedef casacore::MBaseline type;
+  static const std::string name;
+};
+
+template <>
+struct MClass<MeasureRegion::MClass::M_DIRECTION> {
+  typedef casacore::MDirection type;
+  static const std::string name;
+};
+
+template <>
+struct MClass<MeasureRegion::MClass::M_DOPPLER> {
+  typedef casacore::MDoppler type;
+  static const std::string name;
+};
+
+template <>
+struct MClass<MeasureRegion::MClass::M_EARTH_MAGNETIC> {
+  typedef casacore::MEarthMagnetic type;
+  static const std::string name;
+};
+template <>
+struct MClass<MeasureRegion::MClass::M_EPOCH> {
+  typedef casacore::MEpoch type;
+  static const std::string name;
+};
+template <>
+struct MClass<MeasureRegion::MClass::M_FREQUENCY> {
+  typedef casacore::MFrequency type;
+  static const std::string name;
+};
+template <>
+struct MClass<MeasureRegion::MClass::M_POSITION> {
+  typedef casacore::MPosition type;
+  static const std::string name;
+};
+template <>
+struct MClass<MeasureRegion::MClass::M_RADIAL_VELOCITY> {
+  typedef casacore::MRadialVelocity type;
+  static const std::string name;
+};
+template <>
+struct MClass<MeasureRegion::MClass::M_UVW> {
+  typedef casacore::Muvw type;
+  static const std::string name;
+};
+
+#define FOREACH_MEASURE(__func__)                     \
+  __func__(MeasureRegion::MClass::M_BASELINE)         \
+  __func__(MeasureRegion::MClass::M_DIRECTION)        \
+  __func__(MeasureRegion::MClass::M_DOPPLER)          \
+  __func__(MeasureRegion::MClass::M_EARTH_MAGNETIC)   \
+  __func__(MeasureRegion::MClass::M_EPOCH)            \
+  __func__(MeasureRegion::MClass::M_FREQUENCY)        \
+  __func__(MeasureRegion::MClass::M_POSITION)         \
+  __func__(MeasureRegion::MClass::M_RADIAL_VELOCITY)  \
+  __func__(MeasureRegion::MClass::M_UVW)
+
+#define MCLASS_NAME(M) \
+  const std::string MClass<M>::name = MClass<M>::type::showMe();
+FOREACH_MEASURE(MCLASS_NAME)
+#undef MCLASS_NAME
+
+IndexTreeL
+expand_tree(const IndexTreeL& tree, unsigned rank) {
+  IndexTreeL result;
+  auto it = IndexTreeIterator(tree);
+  while (it) {
+    std::vector<coord_t> coords = *it;
+    IndexTreeL ch;
+    for (size_t i = rank; i > 0; --i) {
+      if (i > coords.size()) {
+        ch = IndexTreeL({{0, 1, ch}});
+      } else {
+        ch = IndexTreeL({{coords.back(), 1, ch}});
+        coords.pop_back();
+      }
+    }
+    result.merged_with(ch);
+    ++it;
+  }
+  assert(result.rank());
+  assert(result.rank().value() == rank);
+  return result;
+}
+
+struct MeasureIndexTrees {
+  IndexTreeL metadata_tree;
+  IndexTreeL value_tree;
+};
+
+MeasureIndexTrees
+measure_index_trees(const casacore::Measure& measure) {
+  std::unordered_map<MeasureRegion::ArrayComponent, const casacore::Measure*>
+    components;
+  components[MeasureRegion::ArrayComponent::VALUE] = &measure;
+  auto ref_base = measure.getRefPtr();
+  auto offset = ref_base->offset();
+  components[MeasureRegion::ArrayComponent::OFFSET] = offset;
+  auto frame = ref_base->getFrame();
+  auto epoch = frame.epoch();
+  components[MeasureRegion::ArrayComponent::EPOCH] = epoch;
+  auto position = frame.position();
+  components[MeasureRegion::ArrayComponent::POSITION] = position;
+  auto direction = frame.direction();
+  components[MeasureRegion::ArrayComponent::DIRECTION] = direction;
+  auto radial_velocity = frame.radialVelocity();
+  components[MeasureRegion::ArrayComponent::RADIAL_VELOCITY] = radial_velocity;
+  auto comet = frame.comet();
+  assert(comet == nullptr);
+
+  MeasureIndexTrees result;
+  for (auto& c_m : components) {
+    auto& [c, m] = c_m;
+    MeasureIndexTrees ctrees;
+    switch (c) {
+    case MeasureRegion::ArrayComponent::VALUE:
+      ctrees.metadata_tree = IndexTreeL();
+      ctrees.value_tree =
+        IndexTreeL(
+          components[MeasureRegion::ArrayComponent::VALUE]
+          ->getData()
+          ->getVector()
+          .size());
+      break;
+    default:
+      if (m != nullptr) {
+        ctrees = measure_index_trees(*m);
+      } else {
+        ctrees.metadata_tree = IndexTreeL();
+        ctrees.value_tree = IndexTreeL();
+      }
+      break;
+    }
+    result.metadata_tree =
+      result.metadata_tree.merged_with(
+        IndexTreeL({{c, 1, ctrees.metadata_tree}}));
+    result.value_tree =
+      result.value_tree.merged_with(
+        IndexTreeL({{c, 1, ctrees.value_tree}}));
+  }
+  if (!result.metadata_tree.rank())
+    result.metadata_tree =
+      expand_tree(result.metadata_tree, result.metadata_tree.height() + 1);
+  if (!result.value_tree.rank())
+    result.value_tree =
+      expand_tree(result.value_tree, result.value_tree.height() + 1);
+  return result;
+}
+
+template <int D>
+void
+initialize(
+  PhysicalRegion value_pr,
+  PhysicalRegion metadata_pr,
+  const casacore::Measure& measure) {
+
+  // TODO: remove bounds check on the following accessors
+  const MeasureRegion::ValueAccessor<WRITE_ONLY, D+1, true>
+    vals(value_pr, 0);
+  const MeasureRegion::RefTypeAccessor<WRITE_ONLY, D, true>
+    rtypes(metadata_pr, MeasureRegion::REF_TYPE_FID);
+  const MeasureRegion::MeasureClassAccessor<WRITE_ONLY, D, true>
+    mclasses(metadata_pr, MeasureRegion::MEASURE_CLASS_FID);
+  const MeasureRegion::NumValuesAccessor<WRITE_ONLY, D, true>
+    numvals(metadata_pr, MeasureRegion::NUM_VALUES_FID);
+
+  Point<D + 1> p1;
+  Point<D> p;
+
+  std::stack<
+    std::tuple<const casacore::Measure*, MeasureRegion::ArrayComponent>> ms;
+  ms.push(std::make_tuple(&measure, MeasureRegion::ArrayComponent::VALUE));
+  while (!ms.empty()) {
+    auto& [m, c] = ms.top();
+
+    auto ms_size = ms.size();
+    auto level = ms_size - 1;
+    for (unsigned j = level + 1; j < D; ++j)
+      p1[j] = p1[j] = 0;
+    p1[D] = 0;
+
+    auto ref_base = m->getRefPtr();
+    auto frame = ref_base->getFrame();
+
+    if (c == MeasureRegion::ArrayComponent::VALUE) {
+      // the measure value itself
+      p[level] = p1[level] = MeasureRegion::ArrayComponent::VALUE;
+      auto mvals = m->getData()->getVector();
+      for (unsigned j = 0; j < mvals.size(); ++j) {
+        p1[level + 1] = j;
+        vals[p1] = mvals[j];
+      }
+      rtypes[p] = m->type();
+      numvals[p] = mvals.size();
+      std::string name = m->tellMe();
+      if (name == "") mclasses[p] = MeasureRegion::MClass::M_NONE;
+#define MCLASS(M)                               \
+      else if (name == MClass<M>::name)         \
+        mclasses[p] = M;
+      FOREACH_MEASURE(MCLASS)
+#undef MCLASS
+      else assert(false);
+      c = MeasureRegion::ArrayComponent::OFFSET;
+    }
+
+    while (ms_size == ms.size()
+           && c < MeasureRegion::ArrayComponent::NUM_COMPONENTS) {
+      const casacore::Measure* cm;
+      switch (c) {
+      case MeasureRegion::ArrayComponent::VALUE:
+        assert(false);
+        cm = nullptr;
+        break;
+      case MeasureRegion::ArrayComponent::OFFSET:
+        cm = ref_base->offset();
+        break;
+      case MeasureRegion::ArrayComponent::EPOCH:
+        cm = frame.epoch();
+        break;
+      case MeasureRegion::ArrayComponent::POSITION:
+        cm = frame.position();
+        break;
+      case MeasureRegion::ArrayComponent::DIRECTION:
+        cm = frame.direction();
+        break;
+      case MeasureRegion::ArrayComponent::RADIAL_VELOCITY:
+        cm = frame.radialVelocity();
+        break;
+      default:
+        assert(false);
+        break;
+      }
+      p[level] = p1[level] = c;
+      c = (MeasureRegion::ArrayComponent)((unsigned)c + 1);
+      if (cm != nullptr)
+        ms.push(std::make_tuple(cm, MeasureRegion::ArrayComponent::VALUE));
+      else
+        mclasses[p] = MeasureRegion::MClass::M_NONE;
+    }
+
+    if (ms_size == ms.size())
+      ms.pop();
+  }
+}
+
+template <int D>
+std::unique_ptr<casacore::Measure>
+instantiate(
+  PhysicalRegion value_pr,
+  PhysicalRegion metadata_pr) {
+
+  // TODO: remove bounds check on the following accessors
+  const MeasureRegion::ValueAccessor<READ_ONLY, D+1, true>
+    vals(value_pr, 0);
+  const MeasureRegion::RefTypeAccessor<READ_ONLY, D, true>
+    rtypes(metadata_pr, MeasureRegion::REF_TYPE_FID);
+  const MeasureRegion::MeasureClassAccessor<READ_ONLY, D, true>
+    mclasses(metadata_pr, MeasureRegion::MEASURE_CLASS_FID);
+  const MeasureRegion::NumValuesAccessor<READ_ONLY, D, true>
+    numvals(metadata_pr, MeasureRegion::NUM_VALUES_FID);
+
+  Point<D + 1> p1;
+  Point<D> p;
+
+  std::unique_ptr<casacore::Measure> result;
+  std::stack<
+    std::tuple<
+      std::unique_ptr<casacore::MeasValue>,
+      std::unique_ptr<casacore::MRBase>,
+      MeasureRegion::MClass,
+      MeasureRegion::ArrayComponent>> ms;
+#define PUSH_NEW(s) (s).push(                   \
+    std::make_tuple(                            \
+      std::unique_ptr<casacore::MeasValue>(),   \
+      std::unique_ptr<casacore::MRBase>(),      \
+      MeasureRegion::MClass::M_NONE,            \
+      MeasureRegion::ArrayComponent::VALUE))
+
+  PUSH_NEW(ms);
+  while (!ms.empty()) {
+    auto& [v, r, k, c] = ms.top();
+
+    auto ms_size = ms.size();
+    auto level = ms_size - 1;
+    for (unsigned j = level + 1; j < D; ++j)
+      p1[j] = p1[j] = 0;
+    p1[D] = 0;
+
+    if (c == MeasureRegion::ArrayComponent::VALUE) {
+      // the measure value itself
+      p[level] = p1[level] = MeasureRegion::ArrayComponent::VALUE;
+      k = (MeasureRegion::MClass)mclasses[p];
+      if (k != MeasureRegion::MClass::M_NONE) {
+        casacore::Vector<MeasureRegion::VALUE_TYPE> mvals(numvals[p]);
+        for (unsigned i = 0; i < mvals.size(); ++i) {
+          p1[level + 1] = i;
+          mvals[i] = vals[p1];
+        }
+        switch (k) {
+#define VR(M)                                                       \
+          case M:                                                   \
+            v = std::make_unique<MClass<M>::type::MVType>(mvals);   \
+            r = std::make_unique<MClass<M>::type::Ref>(rtypes[p]);  \
+            break;                                                  
+          FOREACH_MEASURE(VR);
+#undef VR
+        default:
+          assert(false);
+          break;
+        }
+        c = MeasureRegion::ArrayComponent::OFFSET;
+        p[level] = p1[level] = c;
+        PUSH_NEW(ms);
+      } else {
+        ms.pop();
+      }
+    }
+
+    while (ms_size == ms.size()
+           && c < MeasureRegion::ArrayComponent::NUM_COMPONENTS) {
+      std::unique_ptr<casacore::Measure> cm = std::move(result);
+      switch (c) {
+      case MeasureRegion::ArrayComponent::VALUE:
+        assert(false);
+        break;
+      case MeasureRegion::ArrayComponent::OFFSET:
+        if (cm) {
+          switch (k) {
+#define SET_OFFSET(M)                                             \
+            case M:                                               \
+              dynamic_cast<MClass<M>::type::Ref*>(r.get())        \
+                ->set(*dynamic_cast<MClass<M>::type*>(cm.get())); \
+              break;
+            FOREACH_MEASURE(SET_OFFSET);
+#undef SET_OFFSET
+          default:
+            assert(false);
+            break;
+          }
+        }
+        PUSH_NEW(ms);
+        break;
+      case MeasureRegion::ArrayComponent::EPOCH:
+        if (cm)
+          r->getFrame().resetEpoch(*cm);
+        PUSH_NEW(ms);
+        break;
+      case MeasureRegion::ArrayComponent::POSITION:
+        if (cm)
+          r->getFrame().resetPosition(*cm);
+        PUSH_NEW(ms);
+        break;
+      case MeasureRegion::ArrayComponent::DIRECTION:
+        if (cm)
+          r->getFrame().resetDirection(*cm);
+        PUSH_NEW(ms);
+        break;
+      case MeasureRegion::ArrayComponent::RADIAL_VELOCITY:
+        if (cm)
+          r->getFrame().resetRadialVelocity(*cm);
+        switch (k) {
+#define SET_RESULT(M)                                             \
+          case M:                                                 \
+            result =                                              \
+              std::make_unique<MClass<M>::type>(                  \
+                *dynamic_cast<MClass<M>::type::MVType*>(v.get()), \
+                *dynamic_cast<MClass<M>::type::Ref*>(r.get()));   \
+            break;
+          FOREACH_MEASURE(SET_RESULT);
+#undef SET_RESULT
+        default:
+          assert(false);
+          break;
+        }
+        break;
+      default:
+        assert(false);
+        break;
+      }
+      c = (MeasureRegion::ArrayComponent)((unsigned)c + 1);
+      p[level] = p1[level] = c;
+    }
+
+    if (ms_size == ms.size())
+      ms.pop();
+  }
+  return result;
+#undef PUSH_NEW
+}
+
+MeasureRegion
+MeasureRegion::create_like(
+  Context ctx,
+  Runtime* rt,
+  const casacore::Measure& measure) {
+
+  auto index_trees = measure_index_trees(measure);
+
+  LogicalRegion metadata_region;
+  {
+    IndexSpace is = tree_index_space(index_trees.metadata_tree, ctx, rt);
+    FieldSpace fs = rt->create_field_space(ctx);
+    FieldAllocator fa = rt->create_field_allocator(ctx, fs);
+    fa.allocate_field(sizeof(MEASURE_CLASS_TYPE), MEASURE_CLASS_FID);
+    fa.allocate_field(sizeof(REF_TYPE_TYPE), REF_TYPE_FID);
+    fa.allocate_field(sizeof(NUM_VALUES_TYPE), NUM_VALUES_FID);
+    metadata_region = rt->create_logical_region(ctx, is, fs);
+  }
+
+  LogicalRegion value_region;
+  {
+    IndexSpace is = tree_index_space(index_trees.value_tree, ctx, rt);
+    FieldSpace fs = rt->create_field_space(ctx);
+    FieldAllocator fa = rt->create_field_allocator(ctx, fs);
+    fa.allocate_field(sizeof(double), 0);
+    value_region = rt->create_logical_region(ctx, is, fs);  
+  }
+  return MeasureRegion(value_region, metadata_region);
+}
+
+MeasureRegion
+MeasureRegion::create_from(
+  Context ctx,
+  Runtime* rt,
+  const casacore::Measure& measure) {
+
+  MeasureRegion result = create_like(ctx, rt, measure);
+  RegionRequirement
+    value_req(result.value_region, WRITE_ONLY, EXCLUSIVE, result.value_region);
+  value_req.add_field(0);
+  auto value_pr = rt->map_region(ctx, value_req);
+  RegionRequirement
+    metadata_req(
+      result.metadata_region,
+      WRITE_ONLY,
+      EXCLUSIVE,
+      result.metadata_region);
+  metadata_req.add_field(MEASURE_CLASS_FID);
+  metadata_req.add_field(REF_TYPE_FID);
+  metadata_req.add_field(NUM_VALUES_FID);
+  auto metadata_pr = rt->map_region(ctx, metadata_req);
+
+  switch (result.metadata_region.get_dim()) {
+#define INIT(D)                                       \
+    case D:                                           \
+      initialize<D>(value_pr, metadata_pr, measure);  \
+      break;
+    LEGMS_FOREACH_N_LESS_MAX(INIT);
+#undef INIT
+  default:
+    assert(false);
+    break;
+  }
+
+  rt->unmap_region(ctx, metadata_pr);
+  rt->unmap_region(ctx, value_pr);
+
+  return result;
+}
+
+std::unique_ptr<casacore::Measure>
+MeasureRegion::make(Context ctx, Runtime* rt) const {
+
+  RegionRequirement
+    value_req(value_region, READ_ONLY, EXCLUSIVE, value_region);
+  value_req.add_field(0);
+  auto value_pr = rt->map_region(ctx, value_req);
+  RegionRequirement
+    metadata_req(metadata_region, READ_ONLY, EXCLUSIVE, metadata_region);
+  metadata_req.add_field(MEASURE_CLASS_FID);
+  metadata_req.add_field(REF_TYPE_FID);
+  metadata_req.add_field(NUM_VALUES_FID);
+  auto metadata_pr = rt->map_region(ctx, metadata_req);
+
+  std::unique_ptr<casacore::Measure> result;
+  switch (metadata_region.get_dim()) {
+#define INST(D)                                       \
+    case D:                                           \
+      result = instantiate<D>(value_pr, metadata_pr); \
+      break;
+    LEGMS_FOREACH_N_LESS_MAX(INST);
+#undef INST
+  default:
+    assert(false);
+    break;
+  }
+
+  rt->unmap_region(ctx, metadata_pr);
+  rt->unmap_region(ctx, value_pr);
+
+  return result;
+}
+
+void
+MeasureRegion::destroy(Context ctx, Runtime* rt) {
+  for (auto&lr : {&metadata_region, &value_region}) {
+    if (*lr != LogicalRegion::NO_REGION) {
+      rt->destroy_field_space(ctx, lr->get_field_space());
+      rt->destroy_index_space(ctx, lr->get_index_space());
+      *lr = LogicalRegion::NO_REGION;
+    }
+  }
+}
+
+#endif // LEGMS_USE_CASACORE
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// fill-column: 80
+// indent-tabs-mode: nil
+// End:
---
+++ legms/Measures.h
@@ -0,0 +1,140 @@
+#ifndef LEGMS_MEASURES_H_
+#define LEGMS_MEASURES_H_
+
+#include "legms.h"
+#include "utility.h"
+
+#include <memory>
+
+#ifdef LEGMS_USE_CASACORE
+#include <casacore/measures/Measures.h>
+
+namespace legms {
+
+class LEGMS_API MeasureRegion {
+public:
+
+  enum ArrayComponent {
+    VALUE = 0,
+    OFFSET,
+    EPOCH,
+    POSITION,
+    DIRECTION,
+    RADIAL_VELOCITY,
+    // NOT SUPPORTED: COMET, 
+    NUM_COMPONENTS
+  };
+
+  enum MClass {
+    M_BASELINE,
+    M_DIRECTION,
+    M_DOPPLER,
+    M_EARTH_MAGNETIC,
+    M_EPOCH,
+    M_FREQUENCY,
+    M_POSITION,
+    M_RADIAL_VELOCITY,
+    M_UVW,
+    M_NUM_CLASSES,
+    M_NONE = M_NUM_CLASSES
+  };
+
+  typedef casacore::Double VALUE_TYPE;
+  typedef unsigned MEASURE_CLASS_TYPE;
+  typedef unsigned REF_TYPE_TYPE;
+  typedef unsigned NUM_VALUES_TYPE;
+
+  static const constexpr Legion::FieldID MEASURE_CLASS_FID = 0;
+  static const constexpr Legion::FieldID REF_TYPE_FID = 1;
+  static const constexpr Legion::FieldID NUM_VALUES_FID = 2;
+
+  Legion::LogicalRegion value_region;
+  Legion::LogicalRegion metadata_region;
+
+  template <legion_privilege_mode_t MODE, int N, bool CHECK_BOUNDS=false>
+  using ValueAccessor =
+    Legion::FieldAccessor<
+    MODE,
+    VALUE_TYPE,
+    N,
+    Legion::coord_t,
+    Legion::AffineAccessor<VALUE_TYPE, N, Legion::coord_t>,
+    CHECK_BOUNDS>;
+
+  template <legion_privilege_mode_t MODE, int N, bool CHECK_BOUNDS=false>
+  using RefTypeAccessor =
+    Legion::FieldAccessor<
+    MODE,
+    REF_TYPE_TYPE,
+    N,
+    Legion::coord_t,
+    Legion::AffineAccessor<REF_TYPE_TYPE, N, Legion::coord_t>,
+    CHECK_BOUNDS>;
+
+  template <legion_privilege_mode_t MODE, int N, bool CHECK_BOUNDS=false>
+  using MeasureClassAccessor =
+    Legion::FieldAccessor<
+    MODE,
+    MEASURE_CLASS_TYPE,
+    N,
+    Legion::coord_t,
+    Legion::AffineAccessor<MEASURE_CLASS_TYPE, N, Legion::coord_t>,
+    CHECK_BOUNDS>;
+
+  template <legion_privilege_mode_t MODE, int N, bool CHECK_BOUNDS=false>
+  using NumValuesAccessor =
+    Legion::FieldAccessor<
+    MODE,
+    NUM_VALUES_TYPE,
+    N,
+    Legion::coord_t,
+    Legion::AffineAccessor<NUM_VALUES_TYPE, N, Legion::coord_t>,
+    CHECK_BOUNDS>;
+
+  MeasureRegion(
+    Legion::LogicalRegion value_region_,
+    Legion::LogicalRegion metadata_region_)
+    : value_region(value_region_)
+    , metadata_region(metadata_region_) {
+  }
+
+  static MeasureRegion
+  create_like(
+    Legion::Context ctx,
+    Legion::Runtime* rt,
+    const casacore::Measure& measure);
+
+  static MeasureRegion
+  create_from(
+    Legion::Context ctx,
+    Legion::Runtime* rt,
+    const casacore::Measure& measure);
+
+  std::unique_ptr<casacore::Measure>
+  make(Legion::Context ctx, Legion::Runtime* rt) const;
+
+  template <typename M>
+  std::unique_ptr<M>
+  make(Legion::Context ctx, Legion::Runtime* rt) const {
+    std::unique_ptr<casacore::Measure> measure = make(ctx, rt);
+    std::unique_ptr<M> result;
+    if (measure)
+      result.reset(dynamic_cast<M*>(measure.release()));
+    return result;
+  }
+
+  void
+  destroy(Legion::Context ctx, Legion::Runtime* rt);
+};
+
+} // end namespace legms
+
+#endif // LEGMS_USE_CASACORE
+#endif // LEGMS_MEASURES_H_
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// fill-column: 80
+// indent-tabs-mode: nil
+// End:
