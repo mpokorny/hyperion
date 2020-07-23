6563dde4b27df82edf5ebac2b88c575600995894
Martin Pokorny <martin@truffulatree.org>
2020-07-23 10:37:00 -0600

    Add CFTable.cc
    For explicit const constexpr value definitions needed by c++14

---
+++ hyperion/synthesis/CFTable.cc
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 Associated Universities, Inc. Washington DC, USA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#if !HAVE_CXX17
+#include <hyperion/synthesis/CFTable.h>
+
+using namespace hyperion::synthesis;
+
+const constexpr unsigned CFTable::index_rank;
+const constexpr unsigned CFTable::d_x;
+const constexpr unsigned CFTable::d_y;
+#endif
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// fill-column: 80
+// indent-tabs-mode: nil
+// End:
