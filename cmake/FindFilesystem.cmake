3bb5c52dffe23736b721dce96eb2a867e0e6d52b
Martin Pokorny <martin@truffulatree.org>
2020-09-23 15:57:32 -0600

    New FindFilesystem

--- cmake/FindFilesystem.cmake
+++ cmake/FindFilesystem.cmake
@@ -1,7 +1,7 @@
 # Distributed under the OSI-approved BSD 3-Clause License. See
 # https://cmake.org/licensing for details.
 
-# This is copied from:
+# Original version copied from:
 #   https://github.com/vector-of-bool/CMakeCM/blob/master/modules/FindFilesystem.cmake
 
 #[=======================================================================[.rst:
@@ -105,16 +105,31 @@ if(TARGET std::filesystem)
     return()
 endif()
 
+cmake_minimum_required(VERSION 3.10)
+
 include(CMakePushCheckState)
 include(CheckIncludeFileCXX)
-include(CheckCXXSourceCompiles)
+
+# If we're not cross-compiling, try to run test executables.
+# Otherwise, assume that compile + link is a sufficient check.
+if(CMAKE_CROSSCOMPILING)
+    include(CheckCXXSourceCompiles)
+    macro(_cmcm_check_cxx_source code var)
+        check_cxx_source_compiles("${code}" ${var})
+    endmacro()
+else()
+    include(CheckCXXSourceRuns)
+    macro(_cmcm_check_cxx_source code var)
+        check_cxx_source_runs("${code}" ${var})
+    endmacro()
+endif()
 
 cmake_push_check_state()
 
 set(CMAKE_REQUIRED_QUIET ${Filesystem_FIND_QUIETLY})
 
-# All of our tests required C++14 or later
-set(CMAKE_CXX_STANDARD 14)
+# All of our tests required C++17 or later
+set(CMAKE_CXX_STANDARD 17)
 
 # Normalize and check the component list we were given
 set(want_components ${Filesystem_FIND_COMPONENTS})
@@ -162,12 +177,12 @@ if(_CXX_FILESYSTEM_HAVE_HEADER)
     set(_have_fs TRUE)
     set(_fs_header filesystem)
     set(_fs_namespace std::filesystem)
-    set(_cxx_std_17 TRUE)
+    set(_is_experimental FALSE)
 elseif(_CXX_FILESYSTEM_HAVE_EXPERIMENTAL_HEADER)
     set(_have_fs TRUE)
     set(_fs_header experimental/filesystem)
     set(_fs_namespace std::experimental::filesystem)
-    set(_cxx_std_17 FALSE)
+    set(_is_experimental TRUE)
 else()
     set(_have_fs FALSE)
 endif()
@@ -175,62 +190,60 @@ endif()
 set(CXX_FILESYSTEM_HAVE_FS ${_have_fs} CACHE BOOL "TRUE if we have the C++ filesystem headers")
 set(CXX_FILESYSTEM_HEADER ${_fs_header} CACHE STRING "The header that should be included to obtain the filesystem APIs")
 set(CXX_FILESYSTEM_NAMESPACE ${_fs_namespace} CACHE STRING "The C++ namespace that contains the filesystem APIs")
+set(CXX_FILESYSTEM_IS_EXPERIMENTAL ${_is_experimental} CACHE BOOL "TRUE if the C++ filesystem library is the experimental version")
 
 set(_found FALSE)
 
 if(CXX_FILESYSTEM_HAVE_FS)
     # We have some filesystem library available. Do link checks
     string(CONFIGURE [[
+        #include <cstdlib>
         #include <@CXX_FILESYSTEM_HEADER@>
 
         int main() {
             auto cwd = @CXX_FILESYSTEM_NAMESPACE@::current_path();
-            return static_cast<int>(cwd.string().size());
+            printf("%s", cwd.c_str());
+            return EXIT_SUCCESS;
         }
     ]] code @ONLY)
 
-    # Try to compile a simple filesystem program without any linker flags
-    check_cxx_source_compiles("${code}" CXX_FILESYSTEM_NO_LINK_NEEDED)
+    set(prev_libraries ${CMAKE_REQUIRED_LIBRARIES})
 
-    set(can_link ${CXX_FILESYSTEM_NO_LINK_NEEDED})
-
-    if(NOT CXX_FILESYSTEM_NO_LINK_NEEDED)
-        set(prev_libraries ${CMAKE_REQUIRED_LIBRARIES})
-        # Add the libstdc++ flag
-        set(CMAKE_REQUIRED_LIBRARIES ${prev_libraries} -lstdc++fs)
-        check_cxx_source_compiles("${code}" CXX_FILESYSTEM_STDCPPFS_NEEDED)
-        set(can_link ${CXX_FILESYSTEM_STDCPPFS_NEEDED})
-        if(NOT CXX_FILESYSTEM_STDCPPFS_NEEDED)
-            # Try the libc++ flag
-            set(CMAKE_REQUIRED_LIBRARIES ${prev_libraries} -lc++fs)
-            check_cxx_source_compiles("${code}" CXX_FILESYSTEM_CPPFS_NEEDED)
-            set(can_link ${CXX_FILESYSTEM_CPPFS_NEEDED})
+    # Try to compile a simple filesystem program with the libstdc++ flag
+    set(CMAKE_REQUIRED_LIBRARIES ${prev_libraries} -lstdc++fs)
+    _cmcm_check_cxx_source("${code}" CXX_FILESYSTEM_STDCPPFS_NEEDED)
+    set(can_link ${CXX_FILESYSTEM_STDCPPFS_NEEDED})
+    if(NOT CXX_FILESYSTEM_STDCPPFS_NEEDED)
+        # Try to compile a simple filesystem program with the libc++ flag
+        set(CMAKE_REQUIRED_LIBRARIES ${prev_libraries} -lc++fs)
+        _cmcm_check_cxx_source("${code}" CXX_FILESYSTEM_CPPFS_NEEDED)
+        set(can_link ${CXX_FILESYSTEM_CPPFS_NEEDED})
+        if(NOT CXX_FILESYSTEM_CPPFS_NEEDED)
+            # Try to compile a simple filesystem program without any linker flags
+            _cmcm_check_cxx_source("${code}" CXX_FILESYSTEM_NO_LINK_NEEDED)
+            set(can_link ${CXX_FILESYSTEM_NO_LINK_NEEDED})
         endif()
     endif()
 
     if(can_link)
         add_library(std::filesystem INTERFACE IMPORTED)
-        if(_cxx_std_17)
-          target_compile_features(std::filesystem INTERFACE cxx_std_17)
-        else()
-          target_compile_features(std::filesystem INTERFACE cxx_std_14)
-        endif()
+        set_property(TARGET std::filesystem APPEND PROPERTY INTERFACE_COMPILE_FEATURES cxx_std_17)
         set(_found TRUE)
 
         if(CXX_FILESYSTEM_NO_LINK_NEEDED)
             # Nothing to add...
         elseif(CXX_FILESYSTEM_STDCPPFS_NEEDED)
-            target_link_libraries(std::filesystem INTERFACE -lstdc++fs)
+            set_property(TARGET std::filesystem APPEND PROPERTY INTERFACE_LINK_LIBRARIES -lstdc++fs)
         elseif(CXX_FILESYSTEM_CPPFS_NEEDED)
-            target_link_libraries(std::filesystem INTERFACE -lc++fs)
+            set_property(TARGET std::filesystem APPEND PROPERTY INTERFACE_LINK_LIBRARIES -lc++fs)
         endif()
     endif()
 endif()
 
 cmake_pop_check_state()
 
-set(Filesystem_FOUND ${_found} CACHE BOOL "TRUE if we can compile and link a program using std::filesystem" FORCE)
+set(Filesystem_FOUND ${_found} CACHE BOOL "TRUE if we can run a program using std::filesystem" FORCE)
 
 if(Filesystem_FIND_REQUIRED AND NOT Filesystem_FOUND)
-    message(FATAL_ERROR "Cannot Compile simple program using std::filesystem")
+    message(FATAL_ERROR "Cannot run simple program using std::filesystem")
 endif()
